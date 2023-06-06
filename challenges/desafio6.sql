0f92fde87153f0c42b4205665eeab603df551fa7
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-06-06 16:54:20 -0300

    refactor: requisito06

--- challenges/desafio6.sql
+++ challenges/desafio6.sql
@@ -1,7 +1,7 @@
 SELECT
-  FORMAT(MIN(pl.price), 2) AS 'faturamento_minimo',
-  FORMAT(MAX(pl.price), 2) AS 'faturamento_maximo',
-  FORMAT(AVG(pl.price), 2) AS 'faturamento_medio',
-  FORMAT(SUM(pl.price), 2) AS 'faturamento_total'
+  FORMAT(MIN(plans.price), 2) AS 'faturamento_minimo',
+  FORMAT(MAX(plans.price), 2) AS 'faturamento_maximo',
+  FORMAT(AVG(plans.price), 2) AS 'faturamento_medio',
+  FORMAT(SUM(plans.price), 2) AS 'faturamento_total'
 FROM users
   INNER JOIN plans ON plans.plan_id = users.plan_id;
