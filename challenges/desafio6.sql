d057b816d09ba5aaf5e04a1f2cdc1f3defca27fe
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-06-06 16:30:52 -0300

    refactor: requisito03

--- challenges/desafio6.sql
+++ challenges/desafio6.sql
@@ -3,5 +3,5 @@ SELECT
   FORMAT(MAX(pl.price), 2) AS 'faturamento_maximo',
   FORMAT(AVG(pl.price), 2) AS 'faturamento_medio',
   FORMAT(SUM(pl.price), 2) AS 'faturamento_total'
-FROM users AS us
-  INNER JOIN plans as pl ON pl.plan_id = us.plan_id;
+FROM users
+  INNER JOIN plans ON plans.plan_id = users.plan_id;
