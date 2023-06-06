7fe3a9f9cddb7fdc871d78acd2f2fc68c1dc51de
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-06-06 16:37:43 -0300

    refactor: requisito05

--- challenges/desafio5.sql
+++ challenges/desafio5.sql
@@ -1,10 +1,10 @@
 SELECT 
-  s.song_name AS 'cancao', 
-  COUNT(pl.song_id) AS 'reproducoes'
-FROM songs AS s
-  INNER JOIN plays_history AS pl ON pl.song_id = s.song_id
+  songs.song_name AS 'cancao', 
+  COUNT(plays_history.song_id) AS 'reproducoes'
+FROM songs
+  INNER JOIN plays_history ON plays_history.song_id = songs.song_id
 GROUP BY 
-  s.song_name 
+  songs.song_name 
 ORDER BY 
   reproducoes DESC, cancao
 LIMIT 2;
